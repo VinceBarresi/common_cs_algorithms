c7312d8955bdf1351f076273077743a7823e9e91
Vincent Barresi <vbarresi@amplify.com>
2015-07-19 18:24:23 -0400

    remove double quotes from strings

--- search/binary_search.rb
+++ search/binary_search.rb
@@ -1,5 +1,5 @@
 =begin
-Binary Search Algorithm with random a random guess each time the
+Binary Search Algorithm with a random guess each time the
 program is run.
 
 Time Complexity:
@@ -11,26 +11,26 @@ Time Complexity:
 
 min = 0
 primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
-n = primes.sample #random element from array
+n = primes.sample
 max = primes.length - 1
 guess = primes.sample
-puts "selected prime is " + n.to_s
-puts "the first guess is " + guess.to_s
+puts 'selected prime is ' + n.to_s
+puts 'the first guess is ' + guess.to_s
 
 def binary_search min, primes, max, guess, n
   count = 0
   if guess == n 
-    puts "you guessed right!"
-    puts "it took 1 guess to find the prime!"
+    puts 'you guessed right!'
+    puts 'it took 1 guess to find the prime!'
   else
     until guess == n do
         if guess > n
-          puts "Lower"
+          puts 'Lower'
           max = guess - 1
           p guess = (max + min) / 2
           count += 1
         elsif guess < n
-          puts "Higher"
+          puts 'Higher'
           min = guess + 1
           p guess = (max + min) / 2
           count += 1
@@ -38,7 +38,7 @@ def binary_search min, primes, max, guess, n
           break
       end
     end
-      puts "it took " + count.to_s + " guesses to find the prime at index " + primes.index(guess).to_s
+      puts 'it took ' + count.to_s + ' guesses to find the prime at index ' + primes.index(guess).to_s
   end
 end
 
