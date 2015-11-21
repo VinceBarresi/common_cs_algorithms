722ddd12d3d0de158ee82964ec2c4f2f5a737859
Vincent Barresi <vincebarresi@gmail.com>
2015-11-21 17:39:18 -0500

    add bfs class

--- search/breadth_first_search.rb
+++ search/breadth_first_search.rb
@@ -8,23 +8,25 @@ Time Complexity:
 
 require_relative 'node'
 
-def enqueue queue, obj
-  queue.push(obj)
-end
-
-def dequeue queue
-  queue.pop()
-end
-
-def do_bfs queue
-  visited = {queue.first => true}
-  while !queue.empty? do
-    root_node = queue.pop
-    queue.each do |child_node|
-      if visited[child_node] != true
-        puts child_node
-        queue.push(rand(0..1))
-        visited[child_node] = true
+class BFS
+  def enqueue queue, obj
+    queue.push(obj)
+  end
+  
+  def dequeue queue
+    queue.pop()
+  end
+  
+  def do_bfs queue
+    visited = {queue.first => true}
+    while !queue.empty? do
+      root_node = queue.pop
+      queue.each do |child_node|
+        if visited[child_node] != true
+          puts child_node
+          queue.push(rand(0..1))
+          visited[child_node] = true
+        end
       end
     end
   end
